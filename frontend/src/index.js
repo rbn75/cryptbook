7421b542343aea5032d8d9cdc326e89d9228dc59
Luizzmy <luisg.valenzuelam@gmail.com>
2020-12-02 12:01:19 -0600

    header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
 import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css'
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(
+    <Router />, 
+    document.getElementById('root')
+    );
 
 serviceWorker.unregister();
