3b9e00eb71da9638ac6232ba63bbb0debb4e3f52
Rubén Rodríguez <rbn@iMac-de-Ruben.local>
2020-12-02 17:14:39 -0600

    context

---
+++ frontend/src/hooks/context.js
@@ -0,0 +1,36 @@
+import { useState, createContext, useContext, useEffect } from 'react'
+import { currentUserFn } from '../services/auth'
+
+export const DataContext = createContext()
+
+export const DataCtxProvider = props => {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        async function getSessionData() {
+            const { data } = await currentUserFn()
+            login(data);
+        }
+        getSessionData()
+    },[])
+
+    const login = userInfo => {
+        setUser(userInfo)
+
+    }
+
+    const logout = () => {
+        setUser(null)
+    }
+
+    const value = { user, login, logout }
+
+    return (
+        // DataContext.Provider???
+        <DataContext.Provider {...props} value={value} />
+    )
+}
+
+
+export const useContextData = () => useContext(DataContext)
+
