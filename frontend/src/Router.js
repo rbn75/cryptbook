304512f4aaff154b8879247dc74ccf29f8511d28
Rubén Rodríguez <rbn@iMac-de-Ruben.local>
2020-12-02 10:28:20 -0600

    routes define and done

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './components/home/Home';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import Btc from './pages/btc/Btc';
+import Xrp from './pages/xrp/Xrp';
+import Eth from './pages/eth/Eth';
 import NotFound from './components/404/NotFound.js';
+
+
+// testing routes
+// const Profile = () => <h1>profile</h1>
+
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route component={Profile} exact path='/profile' />
+      <Route component={Btc} exact path='/btc' />
+      <Route component={Xrp} exact path='/xrp' />
+      <Route component={Eth} exact path='/eth' />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
---
+++ frontend/src/pages/eth/Eth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Eth() {
+  return (
+    <div>
+      <h1>Eth test</h1>
+    </div>
+  );
+}
+
+export default Eth;
