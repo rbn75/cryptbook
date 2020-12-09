1d9ed537948ade250b341c9537eb14e5da8610bb
Rubén Rodríguez <rbn@iMac-de-Ruben.local>
2020-12-09 11:20:08 -0600

    db

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const passport = require('./config/passport');
 const MongoStore=require('connect-mongo')(session)
 
 mongoose
-  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.DB || { useNewUrlParser: true, useUnifiedTopology: true })
   .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch((err) => console.error('Error connecting to mongo', err));
 
